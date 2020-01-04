3a51f4233f812393b0505fb4a3fbf6f63e1cd402
omerlux <omerlux@post.bgu.ac.il>
2020-01-04 20:40:46 +0200

    Borrow book. not finished Book.java, User.java, UsersControl.java

--- src/main/java/bgu/spl/net/impl/stomp/User.java
+++ src/main/java/bgu/spl/net/impl/stomp/User.java
@@ -12,8 +12,8 @@ public class User {
     private String name;                                            // user name
     private String password;                                        // user password
     private int connectionId;                                       // user connection id which used in the ConnectionImpl
-    private ArrayList<String> topic_list;                           // user lists of topics
-    private HashMap<String, ArrayList<String>> book_inventory;      //user map of genre-bookList
+    private ArrayList<String> topic_subArray;                       // user lists of topics
+    private HashMap<String, ArrayList<Book>> book_inventory;      //user map of genre-bookList
     //------------------- end edit 4/1 --------------------------
 
     public User(String name, String password, int connectionId) {
@@ -21,7 +21,7 @@ public class User {
         this.name = name;
         this.password = password;
         this.connectionId = connectionId;
-        this.topic_list = new ArrayList<>();
+        this.topic_subArray = new ArrayList<>();
         this.book_inventory = new HashMap<>();
         //------------------- end edit 4/1 --------------------------
     }
@@ -50,15 +50,15 @@ public class User {
     /** user topics getter **/
     public ArrayList<String> getTopic_list(){
         //------------------- start edit 4/1 ------------------------
-        return topic_list;
+        return topic_subArray;
         //------------------- end edit 4/1 --------------------------
     }
 
     /** Add topic to user's topic_list **/
     public void addTopic (String topic){
         //------------------- start edit 4/1 ------------------------
-        if(!topic_list.contains(topic)){
-            topic_list.add(topic);
+        if(!topic_subArray.contains(topic)){
+            topic_subArray.add(topic);
         }
         //------------------- end edit 4/1 --------------------------
     }
@@ -66,8 +66,8 @@ public class User {
     /** Remove topic from user's topic_list **/
     public void removeTopic (String topic){
         //------------------- start edit 4/1 ------------------------
-        if(!topic_list.contains(topic)){
-            topic_list.remove(topic);
+        if(!topic_subArray.contains(topic)){
+            topic_subArray.remove(topic);
         }
         //------------------- end edit 4/1 --------------------------
     }
@@ -75,25 +75,24 @@ public class User {
     /** Remove all topics when user logout **/
     public void removeAllTopics (){
         //------------------- start edit 4/1 ------------------------
-        topic_list.clear();
+        topic_subArray.clear();
         //------------------- end edit 4/1 --------------------------
     }
 
     /**
      * Add book to user's book map
      * @param book
-     * @param topic
      * @return true if the book is new.
      *         false if the book is already in this user
      */
-    public boolean addBook (String book, String topic){
+    public boolean addBook (Book book){         /** no book is multi genre **/
         //------------------- start edit 4/1 ------------------------
-        ArrayList<String> tmp_array = new ArrayList<>();
+        ArrayList<Book> tmp_array = new ArrayList<>();
         tmp_array.add(book);
-        if(book_inventory.putIfAbsent(topic, tmp_array) != null){        //PutIfAbsent returns null if there is no topic
+        if(book_inventory.putIfAbsent(book.getTopic(), tmp_array) != null){        //PutIfAbsent returns null if there is no topic
             // that topic exists in the book_inventory map
-            if(!book_inventory.get(topic).contains(book)) {              //checks if already contains book
-                book_inventory.get(topic).add(book);                     // ADDING book to the topic array
+            if(!book_inventory.get(book.getTopic()).contains(book)) {              //checks if already contains book
+                book_inventory.get(book.getTopic()).add(book);                     // ADDING book to the topic array
             }
             return true;                                                 // added a new book
         }
@@ -104,17 +103,31 @@ public class User {
     /**
      * Remove book from user's book map
      * @param book
-     * @param topic
      * @return true if the book has been removed
      *         false if the book isn't in the this user
      */
-    public boolean removeBook (String book, String topic){
+    public boolean removeBook (Book book){
         //------------------- start edit 4/1 ------------------------
-        if(book_inventory.containsKey(topic)){                    // checks if the topic exists in the map
-            book_inventory.get(topic).remove(book);               // REMOVES the book from the topic array
+        if(book_inventory.containsKey(book.getTopic())){                    // checks if the topic exists in the map
+            book_inventory.get(book.getTopic()).remove(book);               // REMOVES the book from the topic array
             return true;                                          // book has been removed successfully
         }
         return false;                                             // No such book
+        //------------------- end edit 4/1 --------------------------
+    }
+
+    /**
+     * This function return true if the book exists in the user inventory
+     * @param book
+     * @return  true - book exists in the user's inventory
+     *          false - book isn't here...
+     */
+    public boolean bookExist (Book book){
+        //------------------- start edit 4/1 ------------------------
+        if (book_inventory.containsKey(book.getTopic()))
+            if (book_inventory.get(book.getTopic()).contains(book))
+                return true;
+        return false;
         //------------------- end edit 4/1 --------------------------
     }
 
